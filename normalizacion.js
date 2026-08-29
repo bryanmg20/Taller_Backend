e5c8ce2e92b93431fd42f06245899495ec8ef995
Alan Garcia <alansg@uninorte.edu.co>
2026-08-29 15:05:22 -0500

    implementar la funcion que normaliza los personajes

--- normalizacion.js
+++ normalizacion.js
@@ -2,13 +2,31 @@ const main = require('./extraccion_secuencial');
 
 const url = 'https://rickandmortyapi.com/api/character';
 
-async function ejecutar() {
+async function normalizar() {
     try {
-        const resultado = await main(url);
-        console.log(resultado);
-    } catch (err) {
-        console.log(err);
+        const datos = await main(url);
+
+        const datosNormalizados = datos.map(personaje => {
+            return {
+                id: personaje.id,
+                nombre: personaje.name,
+                estado: personaje.status,
+                especie: personaje.species,
+                tipo: personaje.type,
+                genero: personaje.gender,
+                origen: personaje.origin.name,
+                ubicacionActual: personaje.location.name,
+                cantidadEpisodios: personaje.episode.length,
+                imagen: personaje.image
+            };
+        });
+        
+        return datosNormalizados;
+        //console.log(datosNormalizados);
+    } catch (error) {
+        console.log(error);
     }
 }
 
-ejecutar();
+module.exports = normalizar;
+//normalizar();
